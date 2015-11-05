f14102c45292bf0da504f4a0cb24af1a3796bb36
fernando.sanchez <fsm_031@hotmail.com>
2015-11-05 14:44:02 +0100

    comienzo del ejercicio2

--- AlumnoB/ejercicio2/ITV.java
+++ AlumnoB/ejercicio2/ITV.java
@@ -2,9 +2,23 @@ package ejercicio2;
 
 public class ITV {
 
+	public int pIspeccion = 0;
+	public int numVehiculos = 0;
+	
 	public static void main(String[] args) {
-		
 
 	}
+
+}
+
+
+
+class Vehiculo extends Thread {
+
+}
+
+
+
+class PInspeccion extends Thread {
 
 }
